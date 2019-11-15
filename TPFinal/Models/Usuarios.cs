6a22493c24a7462d230fcfcf73cbab2ddcb41e89
JULIAN MARCOS CARBALLO <44749323@comunidadort.edu.ar>
2019-11-15 15:37:44 -0300

    no me funcionaron los helpers hasta  las 15:45

--- TPFinal/Models/Usuarios.cs
+++ TPFinal/Models/Usuarios.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace TPFinal.Models
 {
     public class Usuarios
     {
-        private int  _Idusuraio;
-        private string _nombre;
-        private string _apellido;
-        private string _mail;
-        private string _clave;
-
-        public int Idusuraio { get => _Idusuraio; set => _Idusuraio = value; }
-        public string Nombre { get => _nombre; set => _nombre = value; }
-        public string Apellido { get => _apellido; set => _apellido = value; }
-        public string Mail { get => _mail; set => _mail = value; }
-        public string Clave { get => _clave; set => _clave = value; }
+        public int Idusuraio { get; set; }
+        [Required(ErrorMessage = "Ingrese Nombre")]
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Ingrese Contraseña")]
+        public string Clave { get; set; }
     }
 }
